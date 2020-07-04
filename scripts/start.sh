25bee0c2798b91a2f5b93604146e72276b9e01c3
Doug Edgar <dedgar@redhat.com>
2020-07-04 14:14:07 -0700

    ver bump v0.0.2, some parameterization, domain loop

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+echo "pull v0.0.2"
+
 if [ "$PAUSE_ON_START" = "true" ] ; then
   echo
   echo "This container's startup has been paused indefinitely because PAUSE_ON_START has been set."
@@ -9,8 +11,21 @@ if [ "$PAUSE_ON_START" = "true" ] ; then
   done
 fi
 
+CERT_SERVER=""
+
+if [ "$USE_STAGE" = "true" ] ; then
+  CERT_SERVER=$CERT_SERVER_STAGE
+  else
+  CERT_SERVER=$CERT_SERVER_PROD
+fi
+
+echo "Using $CERT_SERVER"
+echo "Processing domains $DOMAIN_LIST"
+
+IFS=', ' read -r -a array <<< "$DOMAIN_LIST"
+
 while true; do
-  for i in "${DOMAIN_LIST[@]}"; do
+  for i in "${array[@]}"; do
         echo "Checking if our LE certificates need to be renewed with Lego"
           /usr/local/bin/lego --tls=true \
           --tls.port=":$TLS_PORT" \
@@ -20,6 +35,7 @@ while true; do
           --filename="$i" \
           --server="$CERT_SERVER" \
           --accept-tos run
+          sleep 60
   done
   sleep 30d
 done
